5558f80d3a63632edf0b474c8fdff636b7bee6e5
kiruha3 <kiruha3@mail.ru>
2023-12-06 18:26:00 +0700

    убрал все репозитории сделал через restTemplate разделил тесты студентов и факультетов

--- src/test/java/ru/hogwarts/hogwarts/StudentRestTemplateApplicationTest.java
+++ src/test/java/ru/hogwarts/hogwarts/StudentRestTemplateApplicationTest.java
@@ -6,24 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import ru.hogwarts.hogwarts.controller.AvatarController;
 import ru.hogwarts.hogwarts.controller.StudentController;
 import ru.hogwarts.hogwarts.model.Faculty;
 import ru.hogwarts.hogwarts.model.Student;
 import ru.hogwarts.hogwarts.repositories.FacultyRepository;
 import ru.hogwarts.hogwarts.repositories.StudentRepository;
-import ru.hogwarts.hogwarts.service.FacultyServiceImpl;
-import ru.hogwarts.hogwarts.service.StudentService;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.boot.test.context.SpringBootTest.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-class HogwartsApplicationTest {
+class StudentRestTemplateApplicationTest {
     @LocalServerPort
     private int port;
     @Autowired
@@ -37,65 +34,8 @@ class HogwartsApplicationTest {
     @Autowired
     private TestRestTemplate testRestTemplate;
 
-    @Test
-    public void contextLoad() throws Exception {
-        Assertions.assertThat(avatarController).isNotNull();
-    }
-
-    @Test
-    public void testGetAvatars() throws Exception {
-        Assertions
-                .assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/avatars", String.class))
-                .isNotNull();
-    }
 
 
-    @Test
-    public void testGetFacultyById() throws Exception {
-        Assertions
-                .assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/faculty" + "/get/", String.class))
-                .isNotNull();
-    }
-
-    @Test
-    public void testPostFaculty() throws Exception {
-        Faculty faculty = new Faculty("Слизерин", "зеленый");
-        Faculty post = this.testRestTemplate.postForObject("http://localhost:" + port + "/faculty", faculty, Faculty.class);
-        Optional resp = this.testRestTemplate.getForObject("http://localhost:" + port + "/faculty/get/" + post.getId(), Optional.class);
-        assertFalse(resp.isEmpty());
-    }
-
-
-    @Test
-    public void testPutFaculty() throws Exception {
-        Faculty faculty = new Faculty("Слизерин", "зеленый");
-        Faculty facultyPut = new Faculty("tor", "зеленый");
-        Faculty post = this.testRestTemplate.postForObject("http://localhost:" + port + "/faculty", faculty, Faculty.class);
-        facultyPut.setId(post.getId());
-        this.testRestTemplate.put("http://localhost:" + port + "/faculty/put/" + post.getId(), facultyPut, Faculty.class);
-        Faculty resp = this.testRestTemplate.getForObject("http://localhost:" + port + "/faculty/get/" + post.getId(), Faculty.class);
-        assertTrue(facultyPut.equals(resp));
-    }
-
-    @Test
-    public void testDeleteFaculty() throws Exception {
-        Faculty faculty = new Faculty("puf", "blue");
-        Faculty post = this.testRestTemplate.postForObject("http://localhost:" + port + "/faculty", faculty, Faculty.class);
-        Optional resp = this.testRestTemplate.getForObject("http://localhost:" + port + "/faculty/get/" + post.getId(), Optional.class);
-        this.testRestTemplate.delete("http://localhost:" + port + "/faculty/remove/" + post.getId());
-        Optional resp1 = this.testRestTemplate.getForObject("http://localhost:" + port + "/faculty/get/" + post.getId(), Optional.class);
-        if (resp1 == null) {
-            assertFalse(resp.equals(resp1));
-        }
-
-    }
-
-    @Test
-    public void testGetStudentsByFacultyId() throws Exception {
-        Assertions
-                .assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/faculty" + "/students-by-faculty-id", String.class))
-                .isNotNull();
-    }
     @Test
     public void testGetStudent() throws Exception {
         Optional resp = this.testRestTemplate.getForObject("http://localhost:" + port + "/students/get/18", Optional.class);
